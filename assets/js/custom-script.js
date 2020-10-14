61659f667c96ab30081f7c9ac5eaad653d91cb29
cah-kangkung <hafizhun.alim93@gmail.com>
2020-10-14 18:05:30 +0700

    Make dataTable responsive

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,35 +1,33 @@
 $(document).ready(function () {
 
     const dataTables = function () {
-        // $('#data-table').DataTable({
-        //     "order": [
-        //         [1, "desc"]
-        //     ]
-        // });
-        $('#dataTable').DataTable();
+        $('#dataTable').DataTable({
+            responsive: true,
+            pageLength: 25
+        });
     }
     dataTables();
 
-    const pusherConnection = function () {
+    // const pusherConnection = function () {
 
-        let pusher = new Pusher('97e23ed5d522856f8f11', {
-            cluster: 'ap1'
-        });
+    //     let pusher = new Pusher('97e23ed5d522856f8f11', {
+    //         cluster: 'ap1'
+    //     });
 
-        let channel = pusher.subscribe('my-channel');
-        channel.bind('my-event', function (data) {
-            $.notify({
-                // options
-                message: data.message,
-                url: 'http://localhost/disc-test/admin_payment',
-            }, {
-                // settings
-                type: 'success'
-            });
-        });
+    //     let channel = pusher.subscribe('my-channel');
+    //     channel.bind('my-event', function (data) {
+    //         $.notify({
+    //             // options
+    //             message: data.message,
+    //             url: 'http://localhost/disc-test/admin_payment',
+    //         }, {
+    //             // settings
+    //             type: 'success'
+    //         });
+    //     });
 
-    }
-    pusherConnection();
+    // }
+    // pusherConnection();
 
 
 
